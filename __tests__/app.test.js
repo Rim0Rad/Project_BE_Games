8e8f4fcca0b5c42e507f9b089e3496ffbf0054da
Rim0Rad <rimas.radziunas5@gmail.com>
2023-05-10 12:21:26 +0100

    4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -59,54 +59,7 @@ describe('GET /api', () => {
     });
 });
 
-describe('GET /api/reviews/:review_id', () => {
-    
-    it('returns status 200', () => {
-        return request(app).get('/api/reviews/1').expect(200)
-    });
 
-    it('returned object has correct properties', () => {
-        return request(app).get('/api/reviews/1')
-        .then( ({ body }) => {
-            expect(body.review).toHaveProperty('review_id')
-            expect(body.review).toHaveProperty('title')
-            expect(body.review).toHaveProperty('review_body')
-            expect(body.review).toHaveProperty('designer')
-            expect(body.review).toHaveProperty('review_img_url')
-            expect(body.review).toHaveProperty('votes')
-            expect(body.review).toHaveProperty('category')
-            expect(body.review).toHaveProperty('owner')
-            expect(body.review).toHaveProperty('created_at')
-        })
-    });
-    it('returned object has correct data', () => {
-        return request(app).get('/api/reviews/2')
-        .then( ({body}) => {
-            expect(body.review.review_id).toBe(2)
-            expect(body.review.title).toBe('Jenga')
-            expect(body.review.designer).toBe('Leslie Scott')
-            expect(body.review.owner).toBe('philippaclaire9')
-            expect(body.review.review_img_url).toBe('https://images.pexels.com/photos/4473494/pexels-photo-4473494.jpeg?w=700&h=700')
-            expect(body.review.review_body).toBe('Fiddly fun for all the family')
-            expect(body.review.category).toBe('dexterity')
-            expect(body.review.votes).toBe(5)
-        })
-    });
-
-    it('returns 404 when given bad id', () => {
-        return request(app).get('/api/reviews/124124214').expect(404)
-        .then( (result) => {
-           expect(result.body.msg).toBe('Review by that ID does not exist')
-        })
-    })
-    it('returns 404 when given bad id', () => {
-        return request(app).get('/api/reviews/hello').expect(400)
-        .then( (result) => {
-            expect(result.body.msg).toBe('Invalid Id')
-         })
-    })
-    
-});
 
 describe('500 Internal Server Error', () => {
     it('returns 500 for internal server error', () => {
--- app.js
+++ app.js
@@ -13,8 +13,6 @@ app.get('/api', getApi)
 /* /api/categories */
 app.get('/api/categories', getCategories)
 
-/* /api/reviews/:review_id */
-app.get('/api/reviews/:review_id', getReviewByID)
 
 app.use((err, req, res, next) => {
     res.status(err.status).send(err)
