29fc9ba5464075f431254245f0f64b0efdc9c518
Rim0Rad <rimas.radziunas5@gmail.com>
2023-05-30 10:15:23 +0100

    allow cross origin recource sharing

--- app.js
+++ app.js
@@ -1,39 +1,31 @@
 const express  = require('express')
 const app = express()
-const { getCategories } = require('./controllers/categories.controller.js')
-const { getApi } = require('./controllers/api.controller.js')
-const { getReviewByID, getReviews, patchReviewById, getReviewComments } = require('./controllers/reviews.controller.js')
-const { deleteComentById, postComment } = require('./controllers/comments.controller.js')
-const { getUsers } = require('./controllers/users.controller.js')
+const apiRouter = require('./routes/api-router')
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json())
 
 /* /api - documentation endpoint that returns and object detailing all available endpoints 
 and posible interactions with them */ 
-app.get('/api', getApi)
+app.use('/api', apiRouter)
+app.use('/api/categories', apiRouter)
+app.use('/api/reviews', apiRouter)
+app.use('/api/reviews/:review_id', apiRouter)
+app.post('/api/reviews/:review_id/comments', apiRouter)
+app.patch('/api/reviews/:review_id', apiRouter)
+app.get('/api/reviews/:review_id/comments', apiRouter)
+app.get('/api/users', apiRouter)
+app.delete('/api/comments/:comment_id', apiRouter)
 
-/* /api/categories */
-app.get('/api/categories', getCategories)
-
-/* /api/reviews/:review_id */
-app.get('/api/reviews/:review_id', getReviewByID)
-
-app.post('/api/reviews/:review_id/comments', postComment)
-
-app.get('/api/reviews/:review_id/comments', getReviewComments)
-/* /api/reviews */
-app.get('/api/reviews', getReviews)
-
-app.get('/api/users', getUsers)
-app.delete('/api/comments/:comment_id', deleteComentById)
-app.patch('/api/reviews/:review_id', patchReviewById)
+/* Invalid endpoint error hanlder*/
+app.use((req, res) => {
+    res.status(404).send({ status: 404, msg: 'Bad Request' })
+})
 
 /* Error hanlder */
 app.use((err, req, res, next) => {
     res.status(err.status).send(err)
-})
-/* Invalid endpoint error hanlder*/
-app.use((req, res) => {
-    res.status(404).send({ status: 404, msg: 'Bad Request' })
 })
 
 module.exports = app
